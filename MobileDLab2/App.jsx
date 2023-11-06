52ef4aa8b9cfc89f7bddf674c2e306765b7cf938
cd-JL <jmasterl@live.com>
2023-11-05 23:57:23 -0700

    First attempt

---
+++ MobileDLab2/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import ToDoList from './ToDoList.jsx';
+import ToDoForm from './ToDoForm.jsx';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <ToDoList />
+      <ToDoForm />
+    </SafeAreaView>
+  );
+}
+
+export default App;
