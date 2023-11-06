afda81a6f99b4f8d2926e2525feb2eaa340d62a0
cd-JL <jmasterl@live.com>
2023-11-06 00:13:18 -0700

    Fixed the error. exported the ToDoForm as ToDoList.

--- MobileDLab2/ToDoForm.jsx
+++ MobileDLab2/ToDoForm.jsx
@@ -48,4 +48,4 @@ const styles = StyleSheet.create({
       },
   });
   
-  export default ToDoList;
+  export default ToDoForm;
--- MobileDLab2/index.js
+++ MobileDLab2/index.js
@@ -4,6 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import ToDoList from './ToDoList';
+import ToDoForm from './ToDoForm';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
